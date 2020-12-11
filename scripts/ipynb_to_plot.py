32e3750ff22636fdd7d694249990ddeee4a49360
Michael Phelan <mphelan@g.harvard.edu>
2020-12-11 16:46:56 -0500

    create percent jupyter notebook from ipynb

--- scripts/ipynb_to_plot.py
+++ scripts/ipynb_to_plot.py
@@ -1,4 +1,5 @@
 import argparse
+import jupytext
 import os
 import re
 
@@ -7,6 +8,7 @@ tgt_fname_divider = '_'
 src_fname_ext = '.ipynb'
 tgt_fname_ext = '.py'
 tgt_fname_prefix = 'plot'
+tgt_nbook_fmt = 'py:percent'
 
 parser = argparse.ArgumentParser()
 parser.add_argument("directory", help="the root directory from which to locate files for transformation")
@@ -20,21 +22,9 @@ for folder, subFolders, files in os.walk(args.directory):
             # replace all filename spaces with underscores
             if(src_fname_divider_bad in tgt_fname_base):
                 tgt_fname_base = tgt_fname_base.replace(src_fname_divider_bad, tgt_fname_divider)
-                # define the target filename using the conventions of fairlearn/autodoc
+            # define the target filename using the conventions of fairlearn/autodoc
             tgt_fname = tgt_fname_prefix + tgt_fname_divider + tgt_fname_base + tgt_fname_ext
-            with open(os.path.join(folder, filename), 'r') as f:
-                newlines = []
-                newlines.append('"""\n')
-                newlines.append('===========================\n')
-                # break up the base filename into words and append them to the docstring
-                newlines.append('OpenDP ' + " ".join(re.split(tgt_fname_divider, tgt_fname_base.title())))
-                newlines.append('\n')
-                newlines.append('===========================\n')
-                newlines.append('"""\n')
-                newlines.append('\n')
-                for line in f.readlines():
-                    newlines.append(line)
-            with open(os.path.join(folder, tgt_fname), 'w') as f:
-                for line in newlines:
-                    f.write(line)
+            src_nbook = jupytext.read(folder + os.sep + filename)
+            jupytext.write(src_nbook, tgt_fname, fmt=tgt_nbook_fmt)
+
 
